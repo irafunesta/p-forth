6bfb7f9dc674030cc04372ceb593966dad13d992
Guidi Simone <gsimone21892@gmail.com>
2025-04-24 12:49:34 +0200

    + testing with branching and ifs

--- compiler.py
+++ compiler.py
@@ -7,9 +7,13 @@ import re
 from pathlib import Path
 
 debug = False
+return_stack = []
+jump_flag = False
+
+
  #fix this
 
-def add(stack):
+def add(stack, word_pointer):
     if len(stack) > 1:
         num1 = stack.pop() 
         num2 = stack.pop()
@@ -20,7 +24,7 @@ def add(stack):
     print("error not enough elements")
     return -1
 
-def emit(stack):
+def emit(stack, word_pointer):
     if len(stack) > 0:
         num1 = stack.pop() 
         print(num1)
@@ -28,12 +32,51 @@ def emit(stack):
     print(stack)
     return 0
 
-def print_stack(stack):
+def print_stack(stack, word_pointer):
     print(stack)
 
-words_dictionary = { "+":add, ".":emit, ".s":print_stack}
+def do_branch(stack, word_pointer):
+    print("do branch")
+    global jump_flag
+    jump_flag = True
+    print("jump_flag :", jump_flag)
+    return 0
 
-def execute_word(stack, word):
+def cond_branch(stack, word_pointer):
+    print("do cond branch")
+    global jump_flag
+    if len(stack) > 0:
+        cond = stack.pop()
+        if cond == 1:
+            jump_flag = True
+        return 0
+    return -1
+
+def save_pointer(stack, word_pointer):
+    return_stack.append(word_pointer)
+    return 0
+
+def ex_if(stack, word_pointer):
+    global jump_flag
+    if len(stack) > 0:
+        condition = stack.pop()
+        if condition != 1:
+            jump_flag = True
+        return 0
+    return -1
+
+def ex_else(stack, word_pointer):
+    #do nothing
+    pass
+
+words_dictionary = { "+":add, ".":emit, ".s":print_stack, 
+    "branch":do_branch,
+    "do":save_pointer,
+    "if":ex_if,
+    "else":ex_else,
+}
+
+def execute_word(stack, word, word_pointer):
     # print("execute word:", word)
     try:
         number = int(word)
@@ -44,7 +87,7 @@ def execute_word(stack, word):
             print(word , " ?")
             return -1
         
-        return code(stack)
+        return code(stack, word_pointer)
     return 0
 
 def get_next_word(words, index):
@@ -53,6 +96,42 @@ def get_next_word(words, index):
     if index > len(words) - 1:
         return None
     return words[index]
+
+def seek(words, word, word_pointer):
+    try:
+        return words.index(word, word_pointer)
+    except ValueError:
+        return -1
+
+# def execute_code(stack, input_code):
+#     word_pointer = 0
+#     words = input_code.strip().split(" ")
+#     print(words)
+
+#     #execution one word at a time
+#     # word = words[word_pointer]
+#     # print("word:", word)
+#     run = True
+#     result = -1
+#     while run:
+#         # print("start loop")
+#         #in case checks for jumps
+#         word = get_next_word(words, word_pointer)
+#         # print("word:", word)
+#         if word != None:
+#             result = execute_word(stack, word)
+#             # print("execution :",integer_stack)
+#             if error == -1 :
+#                 run = False
+#             else:
+#                 if jump_flag and len(return_stack) > 0 :
+#                     word_pointer = return_stack.pop()
+#                 else:
+#                     word_pointer += 1
+#         else:
+#             run = False
+#             result = 0
+#     return result
 
 def interpreter(filename, outputfile, table_name, split, separator) :
     
@@ -67,7 +146,6 @@ def interpreter(filename, outputfile, table_name, split, separator) :
     integer_stack = []
     
     current_program = [] # for now i use words
-    
 
     end = False
     while not end:
@@ -92,13 +170,27 @@ def interpreter(filename, outputfile, table_name, split, separator) :
             word = get_next_word(words, word_pointer)
             # print("word:", word)
             if word != None:
-                error = execute_word(integer_stack, word)
-                # print("execution :",integer_stack)
+
+                #do the seek here ?
+                if word == "if":
+                    #seek and execute else
+                    jump_index = seek(words, 'else', word_pointer)
+                    if jump_index > -1:
+                        return_stack.append(jump_index)
+
+
+                error = execute_word(integer_stack, word, word_pointer)
+                print("stack :",integer_stack)
+                print("return stack :",return_stack)
+                print("jump_flag :", jump_flag)
                 if error == -1 :
                     run = False
                     text_result = ""
                 else:
-                    word_pointer += 1
+                    if jump_flag and len(return_stack) > 0 :
+                        word_pointer = return_stack.pop()
+                    else:
+                        word_pointer += 1
             else:
                 run = False
                 text_result = "Ok"
@@ -108,7 +200,7 @@ def interpreter(filename, outputfile, table_name, split, separator) :
         
     print(f'END ------------------------ ')
 
-# 10 do 1 . loop ||| if x = 1 then 3 else 6 ||| 1 1 cmp 3 else 6
+# 10 do 1 . loop ||| if x = 1 then 3 else 6 ||| if 3 else 6
 # : cmp (n n -- ) (seek for the else, save the index, subtract 2 values, if res is 0, jump to the saved index)
 # need to seek the next word, or the end of the jump 
 
